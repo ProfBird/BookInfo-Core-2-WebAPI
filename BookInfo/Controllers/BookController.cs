7f5730ba1dba655c47662dab9cefe022b1b24990
Brian Bird <ProfBird@users.noreply.github.com>
2019-02-21 13:57:54 -0800

    Conerted controller methods to api endpoints

--- BookInfo/Controllers/BookController.cs
+++ BookInfo/Controllers/BookController.cs
@@ -18,11 +18,17 @@ namespace BookInfo.Controllers
 
         /* Action Methods that get info from the database */
 
-        [Authorize]
-        public ViewResult Index()
+       
+        public IActionResult Index()
         {
             var books = bookRepo.GetAllBooks();
-            return View(books);
+            if (books.Count == 0) {
+                return NotFound();
+            }
+            else
+            {
+                return Ok(books);
+            }
         }
 
         /*  // TODO: Provide views for these actions
@@ -44,7 +50,7 @@ namespace BookInfo.Controllers
         
         /* Action methods that modify the database */
 
-        [Authorize(Roles = "Members")]
+
         [HttpGet]
         public ViewResult Add()
         {
@@ -52,18 +58,64 @@ namespace BookInfo.Controllers
         }
 
         [HttpPost]
-        public RedirectToActionResult Add(string title, string date, string author, string birthdate)
+        public IActionResult Add(string title, string date, string author, string birthdate)
         {
             Book book = new Book { Title = title, Date = DateTime.Parse(date) };
             if (author != null)
             {
                 book.Authors.Add(new Author { Name = author, Birthday = DateTime.Parse(birthdate)});
+                bookRepo.AddBook(book);
+                return Ok(book);
+            }
+            else
+            {
+                return NotFound();  //  TODO: Use a better error method
             }
 
+        }
 
-            bookRepo.AddBook(book);
+        [HttpPut]
+        public IActionResult Replace(int bookId, string title, string date, string author, string birthdate)
+        {
+            Book book = new Book { Title = title, Date = DateTime.Parse(date) };
+            book.BookID = bookId;
+            if (author != null)
+            {
+                book.Authors.Add(new Author { Name = author, Birthday = DateTime.Parse(birthdate) });
+                bookRepo.Edit(book);
+                return Ok(book);
+            }
+            else
+            {
+                return NotFound();  //  TODO: Use a better error method
+            }
 
-            return RedirectToAction("Index");
+        }
+
+        [HttpPatch]
+        public IActionResult Edit(int bookId, string title, string date, string author, string birthdate)
+        {
+            Book book = bookRepo.GetBookById(bookId);
+            if (title != "")
+                book.Title = title;
+            /*
+            if (date != "")
+              //   book.Date = new DateTime(date);
+            if (author != "")
+               // book.Authors[0] = author; // TODO: Specify the author to change
+            if (birthdate != "")
+            //    book.Authors[0].Birthday = birthdate;
+            */
+            if (author != null)
+            {
+                bookRepo.Edit(book);
+                return Ok(book);
+            }
+            else
+            {
+                return NotFound();  //  TODO: Use a better error method
+            }
+
         }
 
         [HttpGet]
